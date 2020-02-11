aa13cb6bbb7274890c9a630f793812b7adc4423a
niaka <whwogjs96@gmail.com>
2020-02-12 00:09:10 +0900

    2.11(1)

---
+++ app/src/main/java/com/example/myapplication/URLS.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class URLS {
+
+    private static final String URL_ROOT = "ec2-54-180-99-222.ap-northeast-2.compute.amazonaws.com";
+    public static final String URL_JOIN = ".join";
+}
