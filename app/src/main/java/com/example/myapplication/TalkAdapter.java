3b447e666047ad0330653f8c90ed90730330389a
suminnn <57792461+suminnn@users.noreply.github.com>
2020-02-12 22:38:18 +0900

    .

--- app/src/main/java/com/example/myapplication/TalkAdapter.java
+++ app/src/main/java/com/example/myapplication/TalkAdapter.java
@@ -50,12 +50,12 @@ public class TalkAdapter extends BaseAdapter {
 
         if(convertView == null){
             LayoutInflater li = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = li.inflate(R.layout.talk_contents_item,parent,false);
+            convertView = li.inflate(R.layout.received_talk,parent,false);
         }
 
-        TextView tci_opposit_id = (TextView) convertView.findViewById(R.id.tci_opposit_id);
-        TextView tci_contents = (TextView) convertView.findViewById(R.id.tci_contents);
-        TextView tci_time = (TextView) convertView.findViewById(R.id.tci_time);
+        TextView tci_opposit_id = (TextView) convertView.findViewById(R.id.received_talk_opposit);
+        TextView tci_contents = (TextView) convertView.findViewById(R.id.received_talk_contents);
+        TextView tci_time = (TextView) convertView.findViewById(R.id.received_talk_time);
 
         TalkItem t = talkItems.get(position);
 
@@ -68,7 +68,30 @@ public class TalkAdapter extends BaseAdapter {
 
     }
 
-    //순서 : 그림 이름 가격 상태
+/*    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Context c = parent.getContext();
+
+        if(convertView == null){
+            LayoutInflater li = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = li.inflate(R.layout.received_talk,parent,false);
+        }
+
+        TextView tci_contents = (TextView) convertView.findViewById(R.id.sent_talk_contents);
+        TextView tci_time = (TextView) convertView.findViewById(R.id.sent_talk_time);
+
+        TalkItem t = talkItems.get(position);
+
+        tci_contents.setText(t.getContents());
+        tci_time.setText(t.getTime());
+
+        return convertView;
+
+
+    }*/
+
+
     public void addTalkItem(String owner_id, String opposit_id, String contents, String time){
         TalkItem t = new TalkItem(owner_id,opposit_id,contents,time);
 
--- app/src/main/java/com/example/myapplication/TalkRoom.java
+++ app/src/main/java/com/example/myapplication/TalkRoom.java
@@ -1,17 +1,22 @@
 package com.example.myapplication;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -22,19 +27,26 @@ import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 public class TalkRoom extends AppCompatActivity {
 
     private static final String TAG = "jsonExample";
+    public static final int LOAD_SUCCESS = 101;
+
     private ListView talk_contents;
     private EditText talk_edit;
     private Button talk_send_btn;
+    private TextView tr_opposit_id;
 
-    String URL = "ec2-54-180-99-222.ap-northeast-2.compute.amazonaws.com";
+    String URL = "http://ec2-54-180-99-222.ap-northeast-2.compute.amazonaws.com:5000/talk_room";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.talk_room);
+
+        tr_opposit_id = (TextView)findViewById(R.id.tr_opposit_id);
 
         talk_contents = (ListView)findViewById(R.id.talk_contents);
         TalkAdapter talkAdapter = new TalkAdapter();
 
         talk_contents.setAdapter(talkAdapter);
+
+        talkAdapter.addTalkItem("나","상대방","내용","시간");
 
         //array.add(talk_edit.getText().toString()); //버튼을 클릭하면 array에 추가
         //talkAdapter.notifyDataSetChanged(); //어댑터 새로고침
@@ -45,14 +57,59 @@ public class TalkRoom extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //getJSON();
+                getJSON();
             }
         });
 
+        talkAdapter.registerDataSetObserver(new DataSetObserver() {
+
+            @Override
+
+            public void onChanged() {
+
+                super.onChanged();
+
+                //talk_contents.setSelection(TalkAdapter.getCount()-1);
+                talk_contents.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+
+            }
+
+        });
     }
 
 
-/*public void getJSON(){
+
+    private final MyHandler mHandler = new MyHandler(this);
+
+
+    private static class MyHandler extends Handler {
+        private final WeakReference<TalkRoom> weakReference;
+
+        public MyHandler(TalkRoom talkRoom) {
+            weakReference = new WeakReference<TalkRoom>(talkRoom);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            TalkRoom talkRoom = weakReference.get();
+
+            if (talkRoom != null) {
+                switch (msg.what) {
+
+                    case LOAD_SUCCESS:
+                        //talkRoom.progressDialog.dismiss();
+
+                        String jsonString = (String)msg.obj;
+
+                        talkRoom.tr_opposit_id.setText(jsonString);//되나볼라고 텍스트뷰암꺼나해봄
+                        break;
+                }
+            }
+        }
+    }
+
+public void getJSON(){
     Thread thread = new Thread(new Runnable() {
 
         public void run() {
@@ -67,7 +124,7 @@ public class TalkRoom extends AppCompatActivity {
 
 
                 httpURLConnection.setReadTimeout(3000);
-                httpURLConnection.setConnectTimeout(3000);
+                httpURLConnection.setConnectTimeout(5000);//3000
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 httpURLConnection.setRequestMethod("GET");
@@ -107,11 +164,12 @@ public class TalkRoom extends AppCompatActivity {
             } catch (Exception e) {
                 result = e.toString();
             }
-
+            Message message = mHandler.obtainMessage(LOAD_SUCCESS, result);
+            mHandler.sendMessage(message);
         }
 
     });
     thread.start();
-}*/
+}
 }
 
