deaf0d308994ed5a2fd8652e1d2e4248a7d59534
niaka <whwogjs96@gmail.com>
2020-02-19 00:40:06 +0900

    2.19

--- app/src/main/java/com/example/myapplication/URLS.java
+++ app/src/main/java/com/example/myapplication/URLS.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 public class URLS {
 
-    private static final String URL_ROOT = "http://ec2-54-180-99-222.ap-northeast-2.compute.amazonaws.com:5000";
+    private static final String URL_ROOT = "http://ec2-54-180-99-222.ap-northeast-2.compute.amazonaws.com:5000/api";
     public static final String URL_JOIN = URL_ROOT + "/join";
+    public static final String URL_CHECK_ID_DUPLICATION = URL_ROOT + "/check_id";
+
 }
---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+public class User {
+
+    private String user_id, user_name, user_phone_number, user_address, user_account;
+
+
+    public User(String user_id, String user_name, String user_phone_number, String user_address, String user_account){
+
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.user_phone_number = user_phone_number;
+        this.user_address = user_address;
+        this.user_account = user_account;
+
+    }
+
+    public String getUser_id() { return user_id; }
+    public String getUser_name() { return user_name; }
+    public String getUser_phone_number() { return user_phone_number; }
+    public String getUser_address() { return user_address; }
+    public String getUser_account() { return user_account; }
+}
